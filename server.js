b4ab05e6132437e959a7a868f3fa1dfeae2d1e6f
luiscarloscdev <luiscarloscordobam@gmail.com>
2018-10-07 21:49:36 -0500

    build

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.static(__dirname + '/dist'));
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/src/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/libelAngular/index.html'));
 });
 
 // Start the app by listening on the default
